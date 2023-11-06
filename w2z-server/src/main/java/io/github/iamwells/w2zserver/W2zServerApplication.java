a4cc04d7d3be2fa3fb19513a3b4ee59e11891bae
iamwells <iamwells@163.com>
2023-11-06 23:47:16 +0800

    update .gitignore and init server

---
+++ w2z-server/src/main/java/io/github/iamwells/w2zserver/W2zServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.iamwells.w2zserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2zServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W2zServerApplication.class, args);
+    }
+
+}
---
+++ w2z-server/src/test/java/io/github/iamwells/w2zserver/W2zServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iamwells.w2zserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2zServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
