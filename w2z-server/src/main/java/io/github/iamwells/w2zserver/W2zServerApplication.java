5327cc66ce42c00551352c709572f42f7446db63
iamwells <iamwells@163.com>
2023-11-07 00:30:31 +0800

    use application.yml & add SecurityConfiguration Class

--- w2z-server/src/main/java/io/github/iamwells/w2zserver/W2zServerApplication.java
+++ w2z-server/src/main/java/io/github/iamwells/w2zserver/W2zServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class W2zServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(W2zServerApplication.class, args);
     }
-
 }
