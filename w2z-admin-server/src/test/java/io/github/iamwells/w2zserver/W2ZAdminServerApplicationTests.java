904728db2c1d021c619e040f512e30a5248ae916
iamwells <iamwells@163.com>
2023-11-24 21:54:56 +0800

    rebuild

--- w2z-admin-server/src/test/java/io/github/iamwells/w2zserver/W2ZAdminServerApplicationTests.java
+++ w2z-admin-server/src/test/java/io/github/iamwells/w2zserver/W2ZAdminServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class W2zServerApplicationTests {
+class W2ZAdminServerApplicationTests {
 
     @Test
     void contextLoads() {
